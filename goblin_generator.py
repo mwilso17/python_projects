68bf77426902da02ccf17e31ae6d78be33370be2
Mike Wilson <mwilso17@my.westga.edu>
2021-06-22 08:34:12 -0400

    added code to create Goblin and print hit_points

--- goblin_generator.py
+++ goblin_generator.py
@@ -7,10 +7,21 @@ from random import randint
 class Goblin:
   """Represents a low level 'goblin' monster that has 3-9 HP."""
 
-  def __init__(self, hit_points):
+  def __init__(self, hit_points=3):
     """Initializes attributes."""
     self.hit_points = hit_points
 
   def create_goblin(self):
     """Simulate the creation of a low level 'goblin'"""
     return randint(3, 9)
+
+
+# Make a goblin.
+goblin_a = Goblin()
+
+hp = []
+for roll_num in range(1):
+  health = goblin_a.create_goblin()
+  hp.append(health)
+
+print(f"\nA goblin has been created and has {hp} hitpoints: ")
