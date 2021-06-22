6a75bf49f2b0f9ead9e35c108270ea6a5bbd6925
Mike Wilson <mwilso17@my.westga.edu>
2021-06-22 08:23:41 -0400

    added object. initialized values. added method

--- goblin_generator.py
+++ goblin_generator.py
@@ -1,3 +1,16 @@
 # Mike Wilson 22 June 2021
 # This program generates a low-level 'goblin' monster to be used in a
-# table top style RPG.
+# table top style RPG. Imports from the Python standard library.
+
+from random import randint
+
+class Goblin:
+  """Represents a low level 'goblin' monster that has 3-9 HP."""
+
+  def __init__(self, hit_points):
+    """Initializes attributes."""
+    self.hit_points = hit_points
+
+  def create_goblin(self):
+    """Simulate the creation of a low level 'goblin'"""
+    return randint(3, 9)
