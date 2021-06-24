46df3d37dca2724e03805296d1de6a5225a968a9
Mike Wilson <mwilso17@my.westga.edu>
2021-06-24 06:44:14 -0400

    added 2 variables

--- kelvin_to_fahrenheit.py
+++ kelvin_to_fahrenheit.py
@@ -1,3 +1,9 @@
 # Mike Wilson 24 June 2021
 # This program converts a given temperature in Kelvin to Fahrenheit.
-# It can also be used to convert Kelvin to Celcius.
+# It can also be used to convert Kelvin to Celsius.
+
+# Definies the temp in Kelvin using an integer.
+kelvin_temp = 301
+
+# Converts the Kelvin temp to Celsius.
+celsius_temp = kelvin_temp - 273.15
