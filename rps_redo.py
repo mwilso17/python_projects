32719688ca810ca7ce2a0a43521a6a0d70b3c490
Mike Wilson <mwilso17@my.westga.edu>
2021-07-05 12:34:55 -0400

    code for computer choice

--- rps_redo.py
+++ rps_redo.py
@@ -1,7 +1,8 @@
 # Mike Wilson 5 July 2021
 # Rock Paper Scissors emulator. 
 
-
+import math
+import random
 
 def get_user_choice(user_input):
   user_input = user_input.lower()
@@ -10,3 +11,11 @@ def get_user_choice(user_input):
   else:
     return 'Please enter a valid option'
 
+def get_computer_choice():
+  random_number = math.floor(random.uniform(0, 3))
+  if random_number == 0:
+    return 'rock'
+  if random_number == 1:
+    return 'paper'
+  if random_number == 2:
+    return 'scissors'
