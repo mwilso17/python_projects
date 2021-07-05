3c5d2f8c2b6bf482635fa1d9b890b0e2ba3fc05a
Mike Wilson <mwilso17@my.westga.edu>
2021-07-05 09:48:03 -0400

    wrote function for rps game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,12 @@
 # Mike Wilson 5 July 2021
 # This program emulates a game of Rock Paper Scissors and allows for user input.
 
+def get_user_choice():
+  """Gets the user's choice"""
+  user_input = input("Please type 'rock', 'paper', or 'scissors'.: ")
+  user_choice = user_input.lower()
+  if user_choice == 'rock' or 'paper' or 'scissors':
+    print(f"You have selected {user_choice}.")
+  else:
+    print("Please select either rock, paper, or scissors.")
+
