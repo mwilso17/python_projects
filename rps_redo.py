ffa9f2c1aa8ee921a331b3db15ad9540b161184f
Mike Wilson <mwilso17@my.westga.edu>
2021-07-05 10:46:25 -0400

    redoing rps for clarity

--- rps_redo.py
+++ rps_redo.py
@@ -1,2 +1,10 @@
 # Mike Wilson 5 July 2021
 # Rock Paper Scissors emulator. 
+
+def get_user_choice(user_input):
+  user_input = user_input.lower()
+  if (user_input == 'rock') or (user_input == 'paper') or (user_input == 'scissors'):
+    return user_input
+  else:
+    return 'Please enter a valid option'
+
