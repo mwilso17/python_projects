6e3ab513a802cf60f5c64345d829c673c0fdb558
Mike Wilson <mwilso17@my.westga.edu>
2021-07-05 10:01:11 -0400

    code for computer selection of rps

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,11 +1,30 @@
 # Mike Wilson 5 July 2021
 # This program emulates a game of Rock Paper Scissors and allows for user input.
 
+import math
+import random
+
 def get_user_choice():
   """Gets the user's choice"""
   user_input = input("Please type 'rock', 'paper', or 'scissors'.: ")
   user_choice = user_input.lower()
   if (user_choice == 'rock') or (user_choice == 'paper') or (user_choice == 'scissors'):
     print(f"You have selected {user_choice}.")
+    return user_choice
   else:
     print("Please restart the program and pick either rock, paper, or scissors.")
+
+def get_computer_choice():
+  """Gets the computers choice of rock, paper, or scissors."""
+  randomNumber = math.floor(random.uniform(0, 3))
+  if randomNumber == 0:
+    print("The computer has selected rock.")
+    return 'rock'
+  if randomNumber == 1:
+    print("The computer has selected paper.")
+    return 'paper'
+  if randomNumber == 2:
+    print("The computer has selected scissors.")
+    return 'scissors'
+
+
