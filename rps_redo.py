8412deeffab289c5dc4b3ef73f0b5e81ad2a9049
Mike Wilson <mwilso17@my.westga.edu>
2021-07-05 10:48:21 -0400

    tested code

--- rps_redo.py
+++ rps_redo.py
@@ -1,6 +1,8 @@
 # Mike Wilson 5 July 2021
 # Rock Paper Scissors emulator. 
 
+
+
 def get_user_choice(user_input):
   user_input = user_input.lower()
   if (user_input == 'rock') or (user_input == 'paper') or (user_input == 'scissors'):
