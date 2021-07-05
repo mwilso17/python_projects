5bfe109da0ef07dfe76b92fc7fc70e3cdb6041e8
Mike Wilson <mwilso17@my.westga.edu>
2021-07-05 09:53:47 -0400

    fixed bug in code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,8 +5,7 @@ def get_user_choice():
   """Gets the user's choice"""
   user_input = input("Please type 'rock', 'paper', or 'scissors'.: ")
   user_choice = user_input.lower()
-  if user_choice == 'rock' or 'paper' or 'scissors':
+  if (user_choice == 'rock') or (user_choice == 'paper') or (user_choice == 'scissors'):
     print(f"You have selected {user_choice}.")
   else:
-    print("Please select either rock, paper, or scissors.")
-
+    print("Please restart the program and pick either rock, paper, or scissors.")
