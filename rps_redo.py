1f5e257462bcaecf42e08ea199bde357175414a9
Mike Wilson <mwilso17@my.westga.edu>
2021-07-05 12:42:40 -0400

    function to determine winner

--- rps_redo.py
+++ rps_redo.py
@@ -19,3 +19,23 @@ def get_computer_choice():
     return 'paper'
   if random_number == 2:
     return 'scissors'
+
+def determine_winner(user_choice, computer_choice):
+  if user_choice == computer_choice:
+    return 'It is a tie'
+  if user_choice == 'rock':
+    if computer_choice == 'paper':
+      return 'Paper covers rock. Computer wins.'
+    else:
+      return 'Rock beats scissors. User wins.'
+  if user_choice == 'paper':
+    if computer_choice == 'scissors':
+      return 'Scissors cut paper. Computer wins.'
+    else:
+      return 'Paper covers rock. User wins.'
+  if user_choice == 'scissors':
+    if computer_choice == 'rock':
+      return 'Rock crushes scissors. Computer wins.'
+    else:
+      return 'Scissors cuts paper. User wins.'
+
