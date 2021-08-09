2abbcf167f0f14406bb731ea1eaa7233424cef85
mwilso17 <michael.b.wilson2@gmail.com>
2021-08-09 08:13:31 -0400

    added files for icalculators and main

---
+++ kiosk/calculators.py
@@ -0,0 +1,2 @@
+# Mike Wilson 9 August 2021
+# This program holds the primary calculating functions for the Kiosk program
---
+++ kiosk/main.py
@@ -0,0 +1,5 @@
+# Mike Wilson 9 August 2021
+# This program executes the main portion of the code for the Kiosk program
+
+import inventory
+import calculators
