5c669a7c1c4578e67e0f0ee3c2bea68df3a501ac
mwilso17 <michael.b.wilson2@gmail.com>
2021-08-09 08:10:57 -0400

    initial inventory

---
+++ kiosk/inventory.py
@@ -0,0 +1,11 @@
+# Mike Wilson 9 August 2021
+# This file contains the inventory list for the kiost mini app
+# inventory is stored in format: 'item name': [barcode, type, price]
+# 'type' is used for purposes of calculating tax.
+
+inventory = {
+  'apple': [1, 'food', 0.59],
+  'soda': [2, 'food', 1.19],
+  'magazine': [3, 'finished product', 6.39],
+  'beer': [4, 'alcohol', 2.19],
+}
