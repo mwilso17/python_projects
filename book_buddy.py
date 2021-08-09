fcb2532d8ad57dfac4edf3ae92ac993b87724406
mwilso17 <michael.b.wilson2@gmail.com>
2021-08-09 16:08:30 -0400

    finishing touches

--- book_buddy.py
+++ book_buddy.py
@@ -5,12 +5,26 @@
 
 filename = 'txt_files\wonderland.txt'
 
-try:
-  with open(filename, encoding='utf-8') as f:
-    contents = f.read()
-except FileNotFoundError:
-  print('File was not found. Please enter a different file')
-else:
-  words = contents.split()
-  num_words = len(words)
-  print(f"The file {filename} has about {num_words} words.")
+def calculate_read_time(filename):
+  reading_speed = input('Enter your reading speed in wpm (200 is average): ')
+  reading_speed = int(reading_speed)
+  time_per_day = input('How many minutes a day do you plan on reading?: ')
+  time_per_day = int(time_per_day)
+
+  try:
+    with open(filename, encoding='utf-8') as f:
+      contents = f.read()
+  except FileNotFoundError:
+    print('File was not found. Please enter a different file')
+  else:
+    words = contents.split()
+    num_words = len(words)
+    print(f"There are roughly {num_words} words in {filename}.")
+
+  days_till_completion = round(num_words/(reading_speed*time_per_day))
+  print(f'It will take you about {days_till_completion} days to finish this text.')
+
+
+
+if __name__ == '__main__':
+  calculate_read_time(filename)
