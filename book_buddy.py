8db60c2860e521294046c118bff3844be1e1f55e
mwilso17 <michael.b.wilson2@gmail.com>
2021-08-09 08:27:41 -0400

    initial code to count words

--- book_buddy.py
+++ book_buddy.py
@@ -3,3 +3,14 @@
 # It also takes user input to determine how long it will take to read the .txt
 # file.
 
+filename = 'txt_files\wonderland.txt'
+
+try:
+  with open(filename, encoding='utf-8') as f:
+    contents = f.read()
+except FileNotFoundError:
+  print('File was not found. Please enter a different file')
+else:
+  words = contents.split()
+  num_words = len(words)
+  print(f"The file {filename} has about {num_words} words.")
