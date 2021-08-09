9592244cd776f41b1e9081c051aabf7176ae7199
mwilso17 <michael.b.wilson2@gmail.com>
2021-08-09 08:15:16 -0400

    new program - book_buddy

---
+++ book_buddy.py
@@ -0,0 +1,5 @@
+# Mike Wilson 9 August 2021
+# This is a mini-application that analyzes a .txt file for its length in words.
+# It also takes user input to determine how long it will take to read the .txt
+# file.
+
