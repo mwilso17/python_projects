14278f80010b60e65703d37067df1d87235a7828
mwilso17 <michael.b.wilson2@gmail.com>
2021-09-13 07:19:35 -0400

    modified code. limited functionality. update later

--- game mechanics/question_npc.py
+++ game mechanics/question_npc.py
@@ -6,36 +6,32 @@
 player_inventory = []
 
 # function to call when the player interacts with/questions an NPC.
-def question_npc():
+def question_npc(questions_asked):
   while True:
-    questions_asked = 0
     if questions_asked == 0:
       print("Welcome traveler! Stay in our small village as long as you want.")
-      questions_asked = questions_asked + 1
-      print(questions_asked)
       break
 
-    elif questions_asked == 1:
+    if questions_asked == 1:
       print("You should probably talk to the village elder.")
-      questions_asked = questions_asked + 1
-      print(questions_asked)
       break
 
 
     if questions_asked == 2:
       print("You can have this. The baker gave me an extra one today.")
       player_inventory.append('sweet roll')
-      questions_asked = questions_asked + 1
-      print(questions_asked)
       break
-
 
     if questions_asked == 3:
       print("Take care, traveler.")
-      print(questions_asked)
       break
 
+# bring up initial inventory
 print(player_inventory)
-question_npc()
-question_npc()
+question_npc(0)
+question_npc(1)
+question_npc(2)
+question_npc(3)
+
+# Bring up inventory again to make sure item was added
 print(player_inventory)
