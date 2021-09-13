5cb790920407acd448715a7fd6c20e36d6c8a1f3
mwilso17 <michael.b.wilson2@gmail.com>
2021-09-13 06:12:15 -0400

    new folder and file

---
+++ game mechanics/README.txt
@@ -0,0 +1,1 @@
+This folder contains files simulate various mechanics seen in video games.
---
+++ game mechanics/question_npc.py
@@ -0,0 +1,3 @@
+# Mike Wilson 13 September 2021
+# This program simulates how a player character may ask a non-player-character
+# (NPC) multiple questions and get a different result each time.
