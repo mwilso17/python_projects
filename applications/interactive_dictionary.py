de9ccbd77c4de0c62e43ec9d3f6856970da66454
mwilso17 <michael.b.wilson2@gmail.com>
2021-11-17 06:12:56 -0500

    accounted for traceback error

--- applications/interactive_dictionary.py
+++ applications/interactive_dictionary.py
@@ -1,2 +1,16 @@
 # Mike Wilson 16 November 2021
 # This program creates an interactive dictionary using python
+# you will need the data.json file located in this folder to run this
+# application properly.
+import json
+data = json.load(open("applications\data.json"))
+
+def translate(word):
+  if word in data:
+    return data[word]
+  else:
+    return "That word doesn't exist. Please check it."
+
+word = input("Enter word: ")
+
+print(translate(word))
