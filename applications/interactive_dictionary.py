6ca07e9db1d06d19699e240c6dc5d0b7d47c1baa
mwilso17 <michael.b.wilson2@gmail.com>
2021-11-17 06:57:34 -0500

    styled final output.

--- applications/interactive_dictionary.py
+++ applications/interactive_dictionary.py
@@ -23,4 +23,10 @@ def translate(word):
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+  for item in output:
+    print(item)
+else:
+  print(output)
