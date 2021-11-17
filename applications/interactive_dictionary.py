ddf33353abf3fb50b4c1cb337746c71c605a86aa
mwilso17 <michael.b.wilson2@gmail.com>
2021-11-17 06:39:07 -0500

    code to check similarity between words

--- applications/interactive_dictionary.py
+++ applications/interactive_dictionary.py
@@ -3,12 +3,15 @@
 # you will need the data.json file located in this folder to run this
 # application properly.
 import json
+from difflib import get_close_matches
 data = json.load(open("applications\data.json"))
 
 def translate(word):
   word = word.lower()
   if word in data:
     return data[word]
+  elif len(get_close_matches(word, data.keys())) > 0:
+    return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
   else:
     return "That word doesn't exist. Please check it."
 
