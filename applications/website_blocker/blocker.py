57a4f003303999321db2c854f17c11af4c2f4db4
mwilso17 <michael.b.wilson2@gmail.com>
2021-11-18 08:01:41 -0500

    new program. initiam comments

---
+++ applications/website_blocker/blocker.py
@@ -0,0 +1,5 @@
+# Mike Wilson 18 November 2021
+# This is a program that blocks websites (as determined by the user) during certain hours
+# For this example, I am using Reddit.com and blocking from 5am until 7am.
+# path for host varies based on OS. C:\Windows\System32\drivers\etc then select host works for Windows
+
