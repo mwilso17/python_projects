c4c9174ff6449cce1b4f49adcac0c791a4051409
mwilso17 <michael.b.wilson2@gmail.com>
2021-11-18 07:16:54 -0500

    new file.

---
+++ applications/webmaps/webmaps.py
@@ -0,0 +1,13 @@
+# Mike Wilson 18 November 2021
+# This program creates an interactive webmap. In progress.
+
+import folium
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+map.add_child(fg)
+
+map.save("Map1.html")
+
+
