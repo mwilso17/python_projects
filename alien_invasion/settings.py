7a11e2d13f1d934ad5c8d43c76cfb4ceb834b361
mwilso17 <michael.b.wilson2@gmail.com>
2021-11-30 07:48:37 -0500

    added settings. adjust main for new settings class

---
+++ alien_invasion/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+  '''A class to store settings for Alien Invasion'''
+  
+  def __init__(self):
+    '''Initialize game settings'''
+    # Screen Settings
+    self.screen_width = 1200
+    self.screen_height = 800
+    self.bg_color = (230, 230, 230)
