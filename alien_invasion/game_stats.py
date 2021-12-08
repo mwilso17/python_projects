49dff1fa76a0ccee6a0ecc36186f5b005ad286ca
mwilso17 <michael.b.wilson2@gmail.com>
2021-12-08 08:07:51 -0500

    added high score

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -8,6 +8,8 @@ class GameStats:
     # Start game in inactive state
     self.game_active = False
 
+    self.high_score = 0
+
   def reset_stats(self):
     self.ships_left = self.settings.ship_limit
     self.score = 0
