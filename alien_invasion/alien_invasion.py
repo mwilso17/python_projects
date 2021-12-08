1e8f6b996d92d043ba754dd9f257f97e8a6bf9ce
mwilso17 <michael.b.wilson2@gmail.com>
2021-12-08 07:02:15 -0500

    added start button

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -29,6 +30,8 @@ class AlienInvasion:
 
     self._create_fleet()
 
+    self.play_button = Button(self, "Play")
+
     # Set background color
     self.bg_color = (230, 230, 230)
 
@@ -41,7 +44,7 @@ class AlienInvasion:
         self.ship.update()
         self._update_bullets()
         self._update_aliens()
-        
+
       self._update_screen()
 
   def _check_events(self):
@@ -53,6 +56,13 @@ class AlienInvasion:
         self._check_keydown_events(event)
       elif event.type == pygame.KEYUP:
         self._check_keyup_events(event)
+      elif event.type == pygame.MOUSEBUTTONDOWN:
+        mouse_pos = pygame.mouse.get_pos()
+        self._check_play_button(mouse_pos)
+
+  def _check_play_button(self, mouse_pos):
+    if self.play_button.rect.collidepoint(mouse_pos):
+      self.stats.game_active = True
 
   def _check_keydown_events(self, event):
     if event.key == pygame.K_RIGHT:
@@ -165,6 +175,9 @@ class AlienInvasion:
     for bullet in self.bullets.sprites():
       bullet.draw_bullet()
     self.aliens.draw(self.screen)
+
+    if not self.stats.game_active:
+      self.play_button.draw_button()
 
     # Make the most recently drawn screen visable
     pygame.display.flip()
--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -5,8 +5,8 @@ class GameStats:
     self.settings = ai_game.settings
     self.reset_stats()
 
-    # Start game in active state
-    self.game_active = True
+    # Start game in inactive state
+    self.game_active = False
 
   def reset_stats(self):
     self.ships_left = self.settings.ship_limit
