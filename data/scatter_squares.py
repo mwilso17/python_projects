431253e7f8cc1f86fa24d2825befcec9122a3b7f
mwilso17 <michael.b.wilson2@gmail.com>
2021-12-21 08:20:09 -0500

    changed color scheme, added range of plots

--- data/scatter_squares.py
+++ data/scatter_squares.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -10,6 +13,8 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
-ax.tick_params(axis='both', which='major', labelsize=14)
+ax.axis([0, 1100, 0, 1100000])
+
+
 
 plt.show()
