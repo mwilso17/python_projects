428a0ac26fae37f70b2d64da586ffe0ec11d770e
mwilso17 <michael.b.wilson2@gmail.com>
2021-12-27 06:37:12 -0500

    allow user to generate multiple walks

--- data/rw_visual.py
+++ data/rw_visual.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
 from random_walk import Randomwalk
 
-# Make a random walk.
-rw = Randomwalk()
-rw.fill_walk()
+while True:
+  # Make a random walk.
+  rw = Randomwalk()
+  rw.fill_walk()
 
-# Plot points in the walk.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+  # Plot points in the walk.
+  plt.style.use('classic')
+  fig, ax = plt.subplots()
+  ax.scatter(rw.x_values, rw.y_values, s=15)
+  plt.show()
+
+  keep_running = input('Make another walk? (y/n): ')
+  if keep_running == 'n':
+    break
