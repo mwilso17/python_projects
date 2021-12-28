46bda7d1dbfb1884c9a173aff0522bcb19581445
mwilso17 <michael.b.wilson2@gmail.com>
2021-12-28 09:04:12 -0500

    added clickable links

--- api/python_repos.py
+++ api/python_repos.py
@@ -12,22 +12,41 @@ print(f'Status code: {r.status_code}')
 # Store API response in a variable
 response_dict = r.json()
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_links, stars, labels = [], [], []
 for repo_dict in repo_dicts:
-  repo_names.append(repo_dict['name'])
+  repo_name = repo_dict['name']
+  repo_url = repo_dict['html_url']
+  repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+  repo_links.append(repo_link)
+
   stars.append(repo_dict['stargazers_count'])
+
+  owner = repo_dict['owner']['login']
+  description = repo_dict['description']
+  label = f"{owner}<br />{description}"
+  labels.append(label)
 
 # Make visualization.
 data = [{
   'type': 'bar',
-  'x': repo_names,
+  'x': repo_links,
   'y': stars,
+  'hovertext': labels,
 }]
 
 my_layout = {
   'title': 'Most starred Python Projects on GitHub',
-  'xaxis': {'title': 'Repository'},
-  'yaxis': {'title': 'Stars'},
+  'titlefont': {'size': 28},
+  'xaxis': {
+    'title': 'Repository',
+    'titlefont': {'size': 24},
+    'tickfont': {'size': 14},
+    },
+  'yaxis': {
+    'title': 'Stars',
+    'titlefont': {'size': 24},
+    'tickfont': {'size': 14},
+    },
 }
 
 fig = {'data': data, 'layout': my_layout}
